0e8277af0010516ccf09aa20c24ecd3fa686d317
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-10-11 10:34:57 +0200

    add route to create list and view

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ListingController::class, 'index']);
+Route::get('/listing/create', [ListingController::class, 'create']);
 Route::get('/listing/{listing}', [ListingController::class, 'show']);
